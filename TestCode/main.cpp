674587ed520726aa7093e1fa42b2f0337f8190bc
moomq <1228819490@qq.com>
2021-05-25 21:01:01 +0800

    add easyloggcc

--- TestCode/main.cpp
+++ TestCode/main.cpp
@@ -4,24 +4,48 @@
 #pragma ide diagnostic ignored "EndlessLoop"
 using namespace std;
 
+#define ELPP_STL_LOGGING
+// #define ELPP_FEATURE_CRASH_LOG -- Stack trace not available for MinGW GCC
+#define ELPP_PERFORMANCE_MICROSECONDS
+#define ELPP_LOG_STD_ARRAY
+#define ELPP_LOG_UNORDERED_MAP
+#define ELPP_UNORDERED_SET
+#include "src/EasyLoggingCpp/easylogging++.h"
+INITIALIZE_EASYLOGGINGPP
+
+//TIMED_SCOPE(appTimer, "myapplication");
+
 void hello()
 {
-    cout<<"hello"<<endl;
+    cout<<"hello1"<<endl;
 }
 
 void good()
 {
-    cout<<"good"<<endl;
+    cout<<"good1"<<endl;
+}
+void easylog()
+{
+    LOG(INFO) << "Test " << __FILE__;
+}
+
+void LogInit()
+{
+    el::Configurations conf("../log_cfg/console.conf");
+    el::Loggers::reconfigureAllLoggers(conf);
 }
 
 /* 填充命令结构体数组 */
 CMD my_cmds[] = {
         {"hello", hello},
         {"good", good},
+        {"easylog",easylog}
 };
 
 int main(int argc, char *argv[]) {
 
+    LogInit();
+    LOG(INFO)<< "My first info log using default logger";
     /* 注册命令 */
     register_cmds(my_cmds, ARRAY_SIZE(my_cmds));
 
